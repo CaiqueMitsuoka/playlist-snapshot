9dbba308abc545046a681daef4b208262563a4c7
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2019-07-03 22:38:58 -0300

    Add RSpotify

--- Gemfile
+++ Gemfile
@@ -27,9 +27,11 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'rspotify'
+
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'dotenv-rails', groups: [:development, :test]
+  gem 'pry'
 end
 
 group :development do
---
+++ config/initializers/rspotify.rb
@@ -0,0 +1,14 @@
+require 'rspotify/oauth'
+
+spotify_client_id = ENV["SPOTIFY_CLIENT_ID"]
+spotify_client_secret = ENV["SPOTIFY_CLIENT_SECRET"]
+
+RSpotify::authenticate(spotify_client_id, spotify_client_secret)
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify,
+    spotify_client_id,
+    spotify_client_secret,
+    scope: 'user-read-email user-library-read user-library-modify' +
+     'playlist-read-collaborative playlist-modify-private playlist-modify-public playlist-read-private'
+end
